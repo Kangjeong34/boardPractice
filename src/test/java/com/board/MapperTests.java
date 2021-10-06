df372d6e933bb8d51979c526e6265d27c279d346
KJH <ikk00171@gmail.com>
2021-10-07 00:02:21 +0900

    게시판 CRUD 처리 테스트 중간

---
+++ src/test/java/com/board/MapperTests.java
@@ -0,0 +1,49 @@
+package com.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.board.domain.BoardDTO;
+import com.board.mapper.BoardMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@SpringBootTest
+class MapperTests {
+
+	@Autowired
+	private BoardMapper boardMapper;
+	
+	@Test
+	public void testOfInsert() {
+		BoardDTO params = new BoardDTO();
+		params.setTitle("1번 게시글 제목");
+		params.setContent("1번 게시글 내용");
+		params.setWriter("테스터");
+		
+		int result = boardMapper.insertBoard(params);
+		System.out.println("결과는" + result + "입니다.");
+	}
+	
+	@Test
+	public void testOfSelectDetail() {
+		BoardDTO board = boardMapper.selectBoardDetail((long) 1);
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.registerModule(new JavaTimeModule());
+			objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+			
+			String boardJson = objectMapper.writeValueAsString(board);
+			
+			System.out.println("=========================");
+			System.out.println(boardJson);
+			System.out.println("=========================");
+			
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	}
+}
